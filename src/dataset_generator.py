c2d0ba02cd675660d2ca32bbb48f5edf6af673ee
Yvan Satyawan <y_satyawan@hotmail.com>
2020-03-31 10:44:27 +0200

    Refactor bin_generator.py into fill_bin.py

---
+++ src/dataset_generator.py
@@ -0,0 +1,12 @@
+"""Dataset Generator.
+
+Generates the dataset. This uses fill_bin.py to generate multiple image
+sequences of a bin being filled. It then generates images that can be saved
+along with annotations in the format specified in README.md
+
+Author:
+    Yvan Satyawan <y_satyawan@hotmail.com>
+
+Created on:
+    March 30, 2020
+"""
